a1edc9851591d724d2bf44172364e27f6ec0a5ba
pritam hooda <hoodap@sheridancollege.ca>
2024-06-23 23:14:38 -0400

    added logic to the sendMimeMessage to send a email message with file attachments and inline imgs if passed as arguments

--- EmailSender/src/main/java/com/pritam/email/sender/EmailSender.java
+++ EmailSender/src/main/java/com/pritam/email/sender/EmailSender.java
@@ -1,14 +1,18 @@
 package com.pritam.email.sender;
 
 import jakarta.annotation.Nullable;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.File;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,8 +25,37 @@ public class EmailSender {
 	private final String UTF_8 = "UTF-8";
 
 	public void sendMimeMessage(InternetAddress[] to, InternetAddress from, String subject, String body, 
-								boolean isBodyHtml, @Nullable File[] attachments, @Nullable File[] imgs){
-		//TODO
+								boolean isBodyHtml, @Nullable File[] attachments, @Nullable Map<String,File> imgs) throws MessagingException{
+		log.info("Sending Mime message...");
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, UTF_8);
+
+		messageHelper.setTo(to);
+		messageHelper.setFrom(from);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(body, isBodyHtml);
+
+		if (attachments != null && attachments.length != 0){
+			for (File attachment : attachments) {
+				messageHelper.addAttachment(attachment.getName(), attachment);
+			}
+		}
+		if (imgs != null && !imgs.isEmpty() ){
+
+			for (Map.Entry<String, File> entry : imgs.entrySet()) {
+				File img = entry.getValue();
+				String contentID = entry.getKey();
+				if (img.exists() ){
+					messageHelper.addInline(contentID, img);
+				}
+				
+			}
+			
+		}
+
+        mailSender.send(message);
+		log.info("Mime message send successfully!");
+
 	}
 
 	public void sendSimpleTextMessage(String to, String from, String subject, String body,
@@ -37,7 +70,7 @@ public class EmailSender {
 			message.setBcc(bcc);
 		}
 		if (cc != null) {
-			message.setBcc(cc);
+			message.setCc(cc);
 		}
 		mailSender.send(message);
 		log.info("Simple Message Sent");
