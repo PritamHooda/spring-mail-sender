0841f2e0b7a143f79197631caca90468a834b7f0
pritam hooda <hoodap@sheridancollege.ca>
2024-07-03 23:45:33 -0400

    updated test class. added initial tests

--- EmailSender/src/test/java/com/pritam/email/sender/EmailSenderTests.java
+++ EmailSender/src/test/java/com/pritam/email/sender/EmailSenderTests.java
@@ -1,13 +1,47 @@
 package com.pritam.email.sender;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.MailSender;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EmailSenderTests {
+	@Autowired
+	EmailSender mailSender;
+
+	private GreenMail greenMail;
+
+	@BeforeAll
+	void init(){
+		greenMail = new GreenMail(ServerSetup.SMTP);
+	}
+
+	@BeforeEach
+	void setup(){
+		greenMail.start();
+	}
+
+	@AfterEach
+	void tearDown(){
+		greenMail.stop();
+	}
 
 	@Test
-	void contextLoads() {
+	public void testSimpleMessage(){
+		mailSender.sendSimpleTextMessage(null, null, null, null, null, null);;
+
+	}
+
+	@SpringBootApplication
+	static class TestConfiguration{
+
 	}
 
 }
